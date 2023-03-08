3b179586f5d890e6b877d5bd651378977d0c1f34
Kevin Lago <kevinthelago@gmail.com>
2023-03-07 21:41:23 -0500

    Initial Commit

---
+++ src/main/java/com/kevinlago/rfg/RfgApplication.java
@@ -0,0 +1,13 @@
+package com.kevinlago.rfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RfgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevinlago/rfg/RfgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinlago.rfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RfgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
